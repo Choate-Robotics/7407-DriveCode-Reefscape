87768af98d5fc4b3ac67561eb1f31ff7b16cb75c
alexl1239 <aluo27@choate.edu>
2025-01-22 14:49:18 -0500

    added subsystem

--- toolkit/subsystem_templates/intake.py
+++ toolkit/subsystem_templates/intake.py
@@ -1,13 +1,16 @@
 from abc import ABC, abstractmethod
+from toolkit.subsystem import Subsystem
 
-class Intake(ABC):
+class Intake(Subsystem, ABC):
+    
     @abstractmethod
     def roll_in(self):
         pass
+
     @abstractmethod
     def roll_out(self):
         pass
+
     @abstractmethod
     def intake_stop(self):
         pass
-    
