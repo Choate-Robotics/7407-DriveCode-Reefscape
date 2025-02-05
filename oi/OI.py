eba4834d3035f0176e72b5377a41a18e917f6b74
lhonney26 <lhonney26@choate.edu>
2025-02-05 15:04:46 -0500

    Started keymap

--- oi/OI.py
+++ oi/OI.py
@@ -14,8 +14,12 @@ class OI:
     @staticmethod
     def map_controls():
         log.info("Mapping controls...")
-        pass
 
         Keymap.Drivetrain.RESET_GYRO.onTrue(
             command.DrivetrainZero(Robot.drivetrain)) \
             .onFalse(command.DriveSwerveCustom(Robot.drivetrain))
+        
+        #INTAKE
+        Keymap.Intake.INTAKE_CORAL.whileTrue(
+            command.
+        )
