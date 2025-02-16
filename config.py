261c604d6af8bfaad58b47d3f7ea7297eebe26b6
Matthew Bardoe <mbardoe@gmail.com>
2025-02-15 21:37:19 -0500

    Added in 2 missing config variables.

--- config.py
+++ config.py
@@ -24,14 +24,14 @@ LOG_FILE_LEVEL: int = 1
 # anything else will log nothing
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-foc_active = False  #foc for TalonFX requires paid subscription
+foc_active = False  # foc for TalonFX requires paid subscription
 
 # DEBUGGING NETWORK TABLES
 NT_INTAKE = False
 NT_ELEVATOR: bool = False
 
 
-#Drivetrain
+# Drivetrain
 gyro_id: int = 13
 
 front_left_move_id: int = 2
@@ -68,22 +68,20 @@ drivetrain_curve: float = 2
 drivetrain_zero: radians = math.radians(180)
 
 TURN_CONFIG = TalonConfig(
-    .9, 0, 0, 0, 0, brake_mode=True,
+    0.9,
+    0,
+    0,
+    0,
+    0,
+    brake_mode=True,
 )
 
 MOVE_CONFIG = TalonConfig(
-    0.11,
-    0,
-    0,
-    0.25,
-    0.01,
-    brake_mode=True,
-    current_limit=40,
-    kV=0.12
+    0.11, 0, 0, 0.25, 0.01, brake_mode=True, current_limit=40, kV=0.12
 )
 
 
-#intake
+# intake
 horizontal_id = 9
 # HORIZONTAL_CONFIG = TalonConfig(
 #     1,
@@ -103,27 +101,23 @@ vertical_id = 13
 
 intake_cancoder_id = 400
 intake_pivot_id = 10
-intake_encoder_zero = 0 #placeholder
-INTAKE_PIVOT_CONFIG = TalonConfig(
-    1,
-    0,
-    0,
-    0,
-    0
-)
+intake_encoder_zero = 0  # placeholder
+INTAKE_PIVOT_CONFIG = TalonConfig(1, 0, 0, 0, 0)
 # placehodler
 intake_max_angle = math.radians(90)
 intake_min_angle = math.radians(60)
 intake_angle_threshold = math.radians(5)
-intake_current_threshold = 80 #placeholder
-intake_current_time_threshold = 2 #placeholder
+intake_current_threshold = 80  # placeholder
+intake_current_time_threshold = 2  # placeholder
 
-intake_max_ff = 0 #placeholder
-intake_ff_offset = 0 #placeholder
+intake_max_ff = 0  # placeholder
+intake_ff_offset = 0  # placeholder
+
+horizontal_intake_speed = 0.3  # placeholder
+vertical_intake_speed = 0.3  # placeholder
 
 
-#elevator
-elevator_lead_id = 10 
+# elevator
+elevator_lead_id = 10
 elevator_follower_id = 11
-magsensor_id = 12 #placeholder
-
+magsensor_id = 12  # placeholder
