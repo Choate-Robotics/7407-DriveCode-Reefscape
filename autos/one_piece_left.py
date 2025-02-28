5bbb9f22c9d2f10df081d5a15b3438b44115d0aa
alexl1239 <aluo27@choate.edu>
2025-02-28 17:06:44 -0500

    updated the autoroutine to one piece commads

--- autos/one_piece_left.py
+++ autos/one_piece_left.py
@@ -7,15 +7,15 @@ from robot_systems import Robot, Field
 from utils.field import get_red_pose
 from command import *
 
+from autos import AutoRoutine
+
 from wpilib import DriverStation
 from commands2 import SequentialCommandGroup, InstantCommand, ParallelCommandGroup
 
 path_name = "One L4 Left"
 paths = [PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(2)]
-starting_pose = get_red_pose(paths[0].getStartingHolonomicPose()) if DriverStation.getAlliance() == DriverStation.Alliance.kRed else paths[0].getStartingHolonomicPose()
 
-auto = SequentialCommandGroup(
-    InstantCommand(lambda: Robot.drivetrain.reset_odometry_auto(starting_pose)),
+command = SequentialCommandGroup(
     InstantCommand(lambda: Robot.wrist.set_coral(True)),
 
     ParallelCommandGroup(
@@ -32,3 +32,5 @@ auto = SequentialCommandGroup(
         Target(config.target_positions["IDLE"], Robot.wrist, Robot.elevator)
     ),
 )
+
+auto = AutoRoutine(command, paths[0].getStartingHolonomicPose())
