ca534705492277eda9e449c961b74a4bb45d279b
alexl1239 <aluo27@choate.edu>
2025-03-25 15:12:06 -0400

    updated the pathing again

--- autos/barge_cetner.py
+++ autos/barge_cetner.py
@@ -17,6 +17,21 @@ paths = [PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(9)]
 
 command = SequentialCommandGroup(
     InstantCommand(lambda: Robot.wrist.set_coral(True)),
+
+    ParallelCommandGroup(
+        # Start to bump to Waypoint
+        AutoBuilder.followPath(paths[0]),
+        Target(config.target_positions["IDLE"], Robot.wrist, Robot.elevator)
+    ),
+    ParallelCommandGroup(
+        # Waypoint to first L4
+        AutoBuilder.followPath(paths[1]).andThen(InstantCommand(lambda: Robot.drivetrain.set_driver_centric((0, 0), 0))),
+        Target(config.target_positions["L4"], Robot.wrist, Robot.elevator),
+    ),
+    FeedOut(Robot.wrist).withTimeout(.3),
+    ParallelCommandGroup(
+        Target(config.target_positions["IDLE"], Robot.wrist, Robot.elevator)
+        )
     )
 
 auto = AutoRoutine(command, paths[0].getStartingHolonomicPose())
