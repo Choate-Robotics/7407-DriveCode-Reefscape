2a25e50c2d5e567f98e19021371644d7c47957b4
alexl1239 <aluo27@choate.edu>
2025-03-26 13:54:59 -0400

    hopefully last thing

--- autos/__init__.py
+++ autos/__init__.py
@@ -3,4 +3,5 @@ from autos.auto_routine import AutoRoutine
 from autos.three_piece_right import auto as three_l4_right
 from autos.three_piece_left import auto as three_l4_left
 from autos.three_l4_left_bump import auto as three_l4_left_bump
+from autos.dealgae_center import auto as dealgae_center
 from autos.leave import auto as leave
