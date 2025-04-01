73eb39927ae2a866b6d7a8fd7616210d48d20dc1
alexl1239 <aluo27@choate.edu>
2025-04-01 15:47:52 -0400

    added center auto

--- autos/dealgae_center.py
+++ autos/dealgae_center.py
@@ -13,7 +13,7 @@ from wpilib import DriverStation
 from commands2 import SequentialCommandGroup, InstantCommand, ParallelCommandGroup, ParallelDeadlineGroup, WaitCommand, ConditionalCommand
 
 path_name = "Center L4 2 Barge Algae"
-paths = [PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(5)]
+paths = [PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(7)]
 
 command = SequentialCommandGroup(
     InstantCommand(lambda: Robot.wrist.set_coral(True)),
@@ -31,22 +31,38 @@ command = SequentialCommandGroup(
     FeedOut(Robot.wrist).withTimeout(.3),
     ParallelCommandGroup(
         Target(config.target_positions["DEALGAE_LOW"], Robot.wrist, Robot.elevator),
-        # Move back a bit and de-algae low
+        # Move back a bit and pick up algae low
         AutoBuilder.followPath(paths[2]).andThen(InstantCommand(lambda: Robot.drivetrain.set_driver_centric((0, 0), 0))),
     ),
+    WristAlgaeIn(Robot.wrist),
     ParallelCommandGroup(
+        # Move to barge
+        AutoBuilder.followPath(paths[3]).andThen(InstantCommand(lambda: Robot.drivetrain.set_driver_centric((0, 0), 0))),
         SequentialCommandGroup(
             WaitCommand(0.2),
-            Target(config.target_positions["IDLE"], Robot.wrist, Robot.elevator),
-        ),
-        # Move back
-        AutoBuilder.followPath(paths[3])
+            Target(config.target_positions["SCORE_BARGE"], Robot.wrist, Robot.elevator),
+        )
     ),
+    WristAlgaeOut(Robot.wrist).withTimeout(0.3),
     ParallelCommandGroup(
-        Target(config.target_positions["DEALGAE_HIGH"], Robot.wrist, Robot.elevator),
-        # Move back a bit and de-algae low
+        # Move to the algae high
         AutoBuilder.followPath(paths[4]).andThen(InstantCommand(lambda: Robot.drivetrain.set_driver_centric((0, 0), 0))),
-    )
-    )
+        SequentialCommandGroup(
+            WaitCommand(0.2),
+            Target(config.target_positions["DEALGAE_HIGH"], Robot.wrist, Robot.elevator),
+        )
+    ),
+    WristAlgaeIn(Robot.wrist),
+    ParallelCommandGroup(
+        # Move to barge
+        AutoBuilder.followPath(paths[5]).andThen(InstantCommand(lambda: Robot.drivetrain.set_driver_centric((0, 0), 0))),
+        SequentialCommandGroup(
+            WaitCommand(0.2),
+            Target(config.target_positions["SCORE_BARGE"], Robot.wrist, Robot.elevator),
+        )
+    ),  
+    WristAlgaeOut(Robot.wrist).withTimeout(0.3),
+    AutoBuilder.followPath(paths[6])
+)
 
 auto = AutoRoutine(command, paths[0].getStartingHolonomicPose())
