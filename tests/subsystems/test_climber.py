153f8fb5ef2cba4a9a9cc8bb4147d76351d2be26
cyrusnaficy <cyrus.naficy@gmail.com>
2025-04-08 14:25:01 -0400

    new climber tests

--- tests/subsystems/test_climber.py
+++ tests/subsystems/test_climber.py
@@ -1,61 +1,40 @@
-# from unittest.mock import MagicMock
+import pytest
+from subsystem import Climber
+from unittest.mock import MagicMock
 
-# import pytest
-# import rev
-# from pytest import MonkeyPatch
+@pytest.fixture
+def climber() -> Climber:
+    climber = Climber()
+    climber.climber_motor = MagicMock()
+    return climber
 
-# import config
-# import constants
-# from subsystem import Climber
-# from toolkit.motors.ctre_motors import TalonFX
+def test_climber_init(climber: Climber):
+    climber.init()
+    climber.climber_motor.init.assert_called()
+    climber.climber_motor.set_sensor_position.assert_called_with(0)
 
-# @pytest.fixture()
-# def climber() -> Climber:
-#     # Create a climber, but it has mock
-#     # classes for its dependencies
-#     my_climber = Climber()
-#     my_climber.climber_motor = MagicMock()
-#     # my_climber.init()
-#     return my_climber
+def test_zero(climber: Climber):
+    climber.zero()
+    climber.climber_motor.set_sensor_position.assert_called_with(0)
+    assert climber.zeroed == True
 
-# def climber_zero(climber: Climber):
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (0.0),
+        (0.5),
+        (1.0),
+        (-0.5)
+    ]
+)
+def test_set_raw_output(test_input, climber: Climber):
+    climber.set_raw_output(test_input)
+    climber.climber_motor.set_raw_output.assert_called_with(test_input)
 
-#     climber.init()
-#     climber.climber_motor.init.assert_called()
+def test_get_motor_revolutions(climber: Climber):
+    assert climber.get_motor_revolutions() == climber.climber_motor.get_sensor_position()
 
-# @pytest.mark.parametrize(
-#     "test_input",
-#     [
-#         (1),
-#         (2),
-#         (3),
-#         (4)
-#     ]
-# )
-
-# def test_set_angle(test_input, climber: Climber):
-#     climber.set_angle(test_input)
-#     climber.climber_motor.set_target_position.assert_called_with(test_input * constants.climber_gear_ratio)
-
-# def test_get_angle(climber: Climber):
-#     climber.get_angle()
-#     climber.climber_motor.get_sensor_position.assert_called()
-
-# @pytest.mark.parametrize(
-#     "test_input",
-#     [
-#         (1),
-#         (2),
-#         (3),
-#         (4)
-#     ]
-# )
-
-# def test_set_raw_output(test_input, climber: Climber):
-#     climber.set_raw_output(test_input)
-#     climber.climber_motor.set_raw_output.assert_called_with(test_input)
-#     climber.climber_motor.set_raw_output.assert_called_with(test_input)
-
-# def test_get_raw_output(climber: Climber):
-#     climber.get_raw_output()
-#     climber.climber_motor.get_applied_output.assert_called()
+def test_update_table(climber: Climber):
+    climber.update_table()
+    climber.climber_motor.get_sensor_position.assert_called()
+    climber.climber_motor.get_motor_current.assert_called()
