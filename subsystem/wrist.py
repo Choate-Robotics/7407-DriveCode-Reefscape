f94a6fba493a3b9b8207ed884391a30e3042af4a
alexl1239 <aluo27@choate.edu>
2025-12-07 22:14:31 -0500

    almost done

--- subsystem/wrist.py
+++ subsystem/wrist.py
@@ -1,40 +1,44 @@
-from phoenix6 import StatusSignal
 from toolkit.subsystem import Subsystem
-from toolkit.motors.ctre_motors import TalonFX
 
 import ntcore
 import math
 from units.SI import radians
 from toolkit.utils.toolkit_math import bounded_angle_diff
-from phoenix6.hardware import CANcoder
+from phoenix6 import hardware, configs, signals, StatusCode, controls
 import config
 import constants
-from wpilib import Timer
 
 
 class Wrist(Subsystem):
     def __init__(self):
         super().__init__()
-        self.feed_motor: TalonFX = TalonFX(
-            config.wrist_feed_id,
-            config.foc_active,
-            inverted=True,
-            config=config.WRIST_FEED_CONFIG,
-        )
-        self.wrist_motor: TalonFX = TalonFX(
-            config.wrist_id,
-            config.foc_active,
-            inverted=True,
-            config=config.WRIST_CONFIG,
-        )
-        self.algae_motor: TalonFX = TalonFX(
-            config.wrist_algae_id,
-            config.foc_active,
-            inverted=False,
-            config=config.WRIST_ALGAE_CONFIG
-        )
 
-        self.encoder: CANcoder = CANcoder(config.wrist_cancoder_id)
+        self.feed_motor: hardware.TalonFX = hardware.TalonFX(config.wrist_feed_id, "rio")
+        self.feed_cfg = configs.TalonFXConfiguration()
+        self.feed_cfg.slot0.k_p = 1.0
+        self.feed_cfg.motor_output.inverted = signals.InvertedValue.COUNTER_CLOCKWISE_POSITIVE
+        self.feed_duty_cycle = controls.DutyCycleOut(0)
+
+
+        self.wrist_motor: hardware.TalonFX = hardware.TalonFX(config.wrist_id, "rio")
+        self.wrist_cfg = configs.TalonFXConfiguration()
+        self.wrist_cfg.slot0.k_p = 48.0
+        self.wrist_cfg.slot0.k_s = 0.06
+        self.wrist_cfg.slot0.k_g = 0.17
+        self.wrist_cfg.motion_magic.motion_magic_cruise_velocity = 97.75
+        self.wrist_cfg.motion_magic.motion_magic_acceleration = 350
+        self.wrist_cfg.feedback.sensor_to_mechanism_ratio = 45
+        self.wrist_cfg.motor_output.inverted = signals.InvertedValue.COUNTER_CLOCKWISE_POSITIVE
+        self.wrist_control = controls.MotionMagicVoltage(0.0)
+
+        self.algae_motor: hardware.TalonFX = hardware.TalonFX(config.wrist_algae_id, "rio")
+        self.algae_cfg = configs.TalonFXConfiguration()
+        self.algae_cfg.slot0.k_p = 1.0
+        self.algae_cfg.motor_output.inverted = signals.InvertedValue.CLOCKWISE_POSITIVE
+        self.algae_duty_cycle = controls.DutyCycleOut(0)
+        self.algae_volts = controls.VoltageOut(0.0)
+
+        self.encoder: hardware.CANcoder = hardware.CANcoder(config.wrist_cancoder_id, "rio")
 
         self.wrist_angle: radians = 0
         self.target_angle: radians = 0
@@ -50,11 +54,21 @@ class Wrist(Subsystem):
         self.algae_running_out: bool = False
 
     def init(self):
-        self.feed_motor.init()
-        self.wrist_motor.init()
-        self.algae_motor.init()
+        self._apply_with_retry(self.feed_motor, self.feed_cfg, "feed")
+        self._apply_with_retry(self.wrist_motor, self.wrist_cfg, "wrist")
+        self._apply_with_retry(self.algae_motor, self.algae_cfg, "algae")
+
         self.initial_zero()
         self.table = ntcore.NetworkTableInstance.getDefault().getTable("wrist")
+
+    def _apply_with_retry(self, motor: hardware.TalonFX, cfg: configs.TalonFXConfiguration, name: str):
+        status: StatusCode = StatusCode.STATUS_CODE_NOT_INITIALIZED
+        for _ in range(5):
+            status = motor.configurator.apply(cfg)
+            if status.is_ok():
+                return
+        # If we get here, it never succeeded
+        print(f"[WristSubsystem] Could not apply config for {name} motor, error code: {status.name}")
 
     def initial_zero(self) -> None:
         """
@@ -62,13 +76,13 @@ class Wrist(Subsystem):
 
         """
         self.wrist_angle = (
-            (self.encoder.get_absolute_position().value - config.wrist_encoder_zero)
+            (self.encoder.get_position() - config.wrist_encoder_zero)
             / constants.wrist_encoder_gear_ratio
             * 2
             * math.pi
         )
 
-        self.wrist_motor.set_sensor_position(
+        self.wrist_motor.get_position(
             self.wrist_angle
             * constants.wrist_gear_ratio / 2 / math.pi
         )
@@ -82,14 +96,14 @@ class Wrist(Subsystem):
         Runs feed motors in to send coral from intake to wrist.
         
         """
-        self.feed_motor.set_raw_output(config.wrist_intake_speed)
+        self.feed_motor.set_control(self.feed_duty_cycle.with_output(config.wrist_intake_speed))
 
     def feed_out(self, speed) -> None:
         """
         Runs feed motors out to score or send coral from wrist to intake.
         
         """
-        self.feed_motor.set_raw_output(speed)
+        self.feed_motor.set_control(self.feed_duty_cycle.with_output(speed))
 
     def feed_stop(self) -> None:
         """
@@ -97,7 +111,7 @@ class Wrist(Subsystem):
         
         """
 
-        self.feed_motor.set_raw_output(0)
+        self.feed_motor.set_control(self.feed_duty_cycle.with_output(0))
 
     def set_coral(self, is_there_a_coral_in_the_feed: bool):
         """
@@ -107,16 +121,16 @@ class Wrist(Subsystem):
         self.coral_in_feed = is_there_a_coral_in_the_feed
 
     def algae_in(self) -> None:
-        self.algae_motor.set_raw_output(config.wrist_algae_speed)
+        self.algae_motor.set_control(self.algae_duty_cycle.with_output(config.wrist_algae_speed))
     
     def algae_out(self) -> None:
-        self.algae_motor.set_raw_output(config.wrist_algae_extake_speed)
+        self.algae_motor.set_control(self.algae_duty_cycle.with_output(config.wrist_algae_extake_speed))
 
     def algae_stop(self) -> None:
-        self.algae_motor.set_raw_output(0)
+        self.algae_motor.set_control(self.algae_duty_cycle.with_output(0))
 
     def hold_algae(self, voltage = config.wrist_algae_hold_volts) -> None:
-        self.algae_motor.set_voltage(voltage)
+        self.algae_motor.set_control(self.algae_volts.with_output(voltage))
 
     def set_algae(self, algae_in_feed: bool) -> None:
         self.algae_in_wrist = algae_in_feed
@@ -152,12 +166,9 @@ class Wrist(Subsystem):
 
         self.target_angle = self.limit_angle(angle)
 
-        ff = config.wrist_max_ff * math.cos(angle - config.wrist_ff_offset)
-
-        self.wrist_motor.set_target_position(
-            (angle / (2 * math.pi)) * constants.wrist_gear_ratio,
-            ff
-        )
+        
+        req = self.wrist_pos_volts.with_position(self.target_angle).with_feed_forward(ff)
+        self.wrist_motor.set_control(req)
 
     def get_wrist_angle(self) -> radians:
         """
@@ -167,7 +178,7 @@ class Wrist(Subsystem):
         
         return (
             (
-                self.wrist_motor.get_sensor_position()
+                self.wrist_motor.get_position()
                 / constants.wrist_gear_ratio
             )
             * math.pi
